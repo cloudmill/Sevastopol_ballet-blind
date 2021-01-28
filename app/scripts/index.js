5734d672a0dbadd28e703e9cfafcd1133f952579
Dmitry68 <iMac@iMac-iMac.local>
2021-01-28 09:56:20 +0300

    first commit

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,4 +3,12 @@ import App from "./main.js";
 
 $(document).ready(function() {
   let app = new App();
+  burger();
 });
+
+function burger() {
+  $('.burger-btn').on('click', function(){
+    $('.burger-btn').toggleClass('burger-btn--active');
+    $('.burger-menu').toggleClass('burger-menu--active');
+  });
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,4 +9,4 @@ export default class App {
   }
 };
 
-export default App;
+// export default App;
