16ae5a4e4bd6d6030f291d34c34bd8ff73b2e3e6
Dmitry68 <iMac@iMac-iMac.local>
2021-01-28 17:57:53 +0300

    add three block

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,42 @@
 import $ from "jquery";
 import App from "./main.js";
+import Swiper from 'swiper';
 
 $(document).ready(function() {
   let app = new App();
   burger();
+  parallax();
+  indexSlider();
 });
 
 function burger() {
   $('.burger-btn').on('click', function(){
     $('.burger-btn').toggleClass('burger-btn--active');
     $('.burger-menu').toggleClass('burger-menu--active');
+  });
+}
+
+function parallax() {
+  $(window).on('scroll', function(){
+    let scroll = $(this).scrollTop();
+
+    $('.parallax').css({
+      'transform': 'translate(0%, ' +  - scroll / 4 + '%'
+    });
+  });
+}
+
+function indexSlider() {
+  var indexSlide = new Swiper('.index__slider-conteiner', {
+    slidesPerView: 'auto',
+    centerSlides: true,
+    pagination: {
+      el: '.index__slider-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.index__next',
+      prevEl: '.index__prev',
+    },
   });
 }
