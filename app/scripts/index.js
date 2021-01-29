0e71c818e7a2c071fae2da87d06d01240d52c835
Dmitry68 <iMac@iMac-iMac.local>
2021-01-29 18:35:43 +0300

    index

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
   burger();
   parallax();
   indexSlider();
+  institSlider();
+  gallery();
+  mediaSlider();
+  headerScroll();
 });
 
 function burger() {
@@ -21,10 +25,46 @@ function parallax() {
     let scroll = $(this).scrollTop();
 
     $('.parallax').css({
-      'transform': 'translate(0%, ' +  - scroll / 4 + '%'
+      'transform': 'translate(0%, ' +  - scroll / 4 + '%)'
     });
   });
 }
+
+function headerScroll() {
+
+  var 
+      header = $('.header-white'),
+      prevScroll = 0,
+      total = 0;
+  $(window).on('scroll', function(){
+    let winScroll = $(this).scrollTop();
+    total = winScroll - prevScroll;
+    prevScroll = winScroll;
+    console.log(total);
+    if ( total > 0){
+      let i = 0;
+      while (i < 400){
+        header.css({
+          'transform': 'translateY(' + -i / 4 + '%)'
+        });
+        i = i + total;
+      }
+    }else{
+      let i = 0;
+      while (i < -400){
+        header.css({
+          'transform': 'translateY(' + (i / 4) * -1 + '%)'
+        });
+        i = i - total;
+      }
+    }
+
+  });
+}
+
+
+
+
 
 function indexSlider() {
   var indexSlide = new Swiper('.index__slider-conteiner', {
@@ -37,6 +77,52 @@ function indexSlider() {
     navigation: {
       nextEl: '.index__next',
       prevEl: '.index__prev',
+    },
+  });
+}
+
+function institSlider() {
+  var institSlide = new Swiper('.index__instit-slider', {
+    slidesPerView: 'auto',
+    centerSlides: true,
+    pagination: {
+      el: '.index__instit-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.instit__next',
+      prevEl: '.instit__prev',
+    },
+  });
+}
+
+function gallery() {
+  var gallerySlide = new Swiper('.index__gallery-conteiner', {
+    // effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    // coverflowEffect: {
+    //   rotate: 0,
+    //   stretch: 0,
+    //   depth: 1000,
+    //   modifier: 1,
+    //   slideShadows: false,
+    // },
+    navigation: {
+      nextEl: '.gallery-next',
+      prevEl: '.gallery-prev',
+    },
+  });
+}
+
+function mediaSlider() {
+  var institSlide = new Swiper('.index__media-conteiner', {
+    slidesPerView: 'auto',
+    centerSlides: true,
+    navigation: {
+      nextEl: '.media-prev',
+      prevEl: '.media-next',
     },
   });
 }
