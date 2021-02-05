384e005015003da97190bff5ee5755f9bd81a2c0
Dmitry68 <iMac@iMac-iMac.local>
2021-02-05 17:36:27 +0300

    add 404, animation

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,11 @@
 import $, { event } from "jquery";
 import App from "./main.js";
 import Swiper from 'swiper';
+import AOS from 'aos';
 
 $(document).ready(function() {
   let app = new App();
+  inAos();
   burger();
   parallax();
   indexSlider();
@@ -14,7 +16,34 @@ $(document).ready(function() {
   headerScroll();
   search();
   close();
+  indexTooltip();
+  infoScroll();
 });
+
+function inAos() {
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
+}
 
 function burger() {
   $('.burger-btn').on('click', function(){
@@ -61,6 +90,22 @@ close = function(){
   });
 }
 
+function indexTooltip(){
+
+  $('.sup-img').on('mouseenter', function(){
+      $('.tooltip').addClass('tooltip--active');
+  });
+  $('.sup-img').on('mouseleave', function(){
+    $('.tooltip').removeClass('tooltip--active');
+  });
+}
+
+function infoScroll() {
+  let top = $('#info').offset().top;
+  $('.index__start-mouse').on('click', function(){
+    $('body,html').animate({scrollTop: top}, 1000);
+  });
+}
 
 function headerScroll() {
 
